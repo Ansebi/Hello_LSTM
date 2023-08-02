6937ade39ad0f4a6fecd656c487ce1fec118bf91
Ansebi <43926760+Ansebi@users.noreply.github.com>
2023-08-02 11:56:36 +0300

    Create cnn_lstm_snippet.py

---
+++ cnn_lstm_snippet.py
@@ -0,0 +1,39 @@
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+    X, y = list(), list()
+    for i in range(len(sequence)):
+        # find the end of this pattern
+        end_ix = i + n_steps
+        # check if we are beyond the sequence
+        if end_ix > len(sequence)-1:
+            break
+        # gather input and output parts of the pattern
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
+ 
+# define input sequence
+n_features = 1
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# choose a number of time steps
+n_steps = 4
+# split into samples
+X, y = split_sequence(raw_seq, n_steps)
+# reshape from [samples, timesteps] into [samples, subsequences, timesteps, features]
+X = X.astype(float).reshape((len(X), n_steps, n_features))
+
+# define model
+model = Sequential()
+model.add(Conv1D(filters=64, kernel_size=1, activation='relu'))
+model.add(AveragePooling1D(pool_size=1))
+model.add(LSTM(256, activation='relu'))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+# fit model
+model.fit(X, y, epochs=500, verbose=0)
+# demonstrate prediction
+x_input = np.array([60, 70, 80, 90])
+x_input = x_input.reshape((1, n_steps, n_features))
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
